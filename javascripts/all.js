94b8090dec7bff698d4248ef828ff8c2619497cc
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-03-08 15:33:14 +0900

    Automated commit at 2015-03-08 06:33:14 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,2 +1,8 @@
-//
+if (! ("import" in document.createElement('link'))) {
+    alert("Live demos in this page don't work because the browser doesn't support HTML Imports. Call for a repot to browser vendor.\nお使いのブラウザーではHTML Importsの機能がサポートされていないため、このページのデモは動作しません。ブラウザーの製造元にお問い合わせください。");
+}
+
+if (! ("WeakMap" in window)) {
+    alert("Live demos in this page don't work because the browser doesn't support WeakMap. Call for a repot to browser vendor.\nお使いのブラウザーではWeakMapの機能がサポートされていないため、このページのデモは動作しません。ブラウザーの製造元にお問い合わせください。");
+}
 ;
